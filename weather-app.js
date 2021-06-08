c1bc908977e924a567952062dca470b1d188421e
nikpdr <nlm_217@yahoo.com>
2021-06-07 18:38:39 -0700

    Added forecast API call to JS

--- weather-app.js
+++ weather-app.js
@@ -45,6 +45,13 @@ function formatDate(timestamp) {
   return `${day} ${month} ${currentDate} ${hours}:${minutes}`;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "b3588d10137e7691b17d27af3f8dd9d5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   console.log(response.data);
   let weatherIcon = document.querySelector("#weather-icon");
@@ -70,9 +77,12 @@ function showTemperature(response) {
   );
   celsiusTemperature = response.data.main.temp;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  getForecast(response.data.coord);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
